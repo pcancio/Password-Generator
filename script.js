514cb300e8482e9924c2b8bf48e372d80165023a
pcancio <89603342+pcancio@users.noreply.github.com>
2021-10-23 20:45:54 -0400

    Add files via upload
    assigned variables

--- script.js
+++ script.js
@@ -16,6 +16,28 @@ const totalLength = prompt("How many characters would you like to use?");
 if(totalLength >= 8 && totalLength <= 128) {
   alert(`Your new password will be ${totalLength} characters long.`)
 }
+
+let upper = confirm("Would you like to include upper case letters");
+let lower = confirm("Would you like to include lower case letters?");
+let symbol = confirm("Would you like to include special characters?");
+let number = confirm("Would you like to include numbers?");
+
+let allCharacters = ""; 
+
+if(upper) {
+  allCharacters += "ABCDEFGHIJKFLMNOPQRSTUVWXYZ";
+}
+if(lower) {
+  allCharacters += "abcdefghijklmnopqrstuvwxyz";
+}
+if(symbol) {
+  allCharacters += "'!#$5&()*+,-./:;<=>?@^[\\]^_`{|}~'";
+}
+if(number) {
+  allCharacters += "0123456789";
+}
+// generate password that matches selected criteria
+ 
 }
 
 // Write password to the #password input
