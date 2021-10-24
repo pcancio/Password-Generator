a91633278c5ba8547febd0f924fa4ee287650567
pcancio <89603342+pcancio@users.noreply.github.com>
2021-10-23 20:18:41 -0400

    Add files via upload
    added logic to password length

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// "ABCDEFGHIJKFLMNOPQRSTUVWXYZ";
+
+// "abcdefghijklmnopqrstuvwxyz";
+
+// "!#$5&()*+,-./:;<=>?@^[\\]^_`{|}~";
+
+// "0123456789";
+
+
+// Generate password
+function generatePassword() {
+const totalLength = prompt("How many characters would you like to use?");
+if(totalLength >= 8 && totalLength <= 128) {
+  alert(`Your new password will be ${totalLength} characters long.`)
+}
+}
 
 // Write password to the #password input
 function writePassword() {
